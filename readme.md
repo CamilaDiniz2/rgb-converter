0e077f71a32e7ee17b7c2c462cb40cc544951880
CamilaDiniz2 <camilla.a.diniz@gmail.com>
2020-07-13 11:00:38 -0300

    Primeira versão do projeto rgb-converter

---
+++ readme.md
@@ -0,0 +1,51 @@
+<h1 align="center">Obtain Colors from the RGB code</h1>
+
+## Índice
+
+- [Sobre](#-sobre)
+- [Resultado](#-resultado)
+- [Tecnologias](#-tecnologias)
+- [Fontes](#-fontes)
+
+---
+
+### Sobre
+
+Código desenvolvido como uma das atividades do [BootCampIGTI](https://www.igti.com.br/) de Desenvolvedor FUll Stack. O trabalho consistia em criar um código, usando HTML, CSS e Javascript, onde um valor RGB era setado e aparecia a cor correspondente.
+
+---
+
+### Resultado
+
+O resultado obtido pode ser visto na figura 1:
+
+<figure class="image">
+  <img src="images/tela-inicial.png" alt="tela inicial">
+  <figcaption align="center">Figura 1: Tela Inicial da Aplicação.</figcaption>
+</figure>
+
+A figura 2 mostra um exemplo da aplicação:
+
+<figure class="image">
+  <img src="images/example.png" alt="tela inicial">
+  <figcaption align="center">Figura 2: Exemplo da aplicação.</figcaption>
+</figure>
+
+---
+
+### Tecnologias
+
+Foram usadas as seguintes tecnologias:
+
+- HTML
+- CSS
+- Javascript
+- Bootstrap
+
+---
+
+### Fontes
+
+- O código para converter de rgb - hexadecimal foi obtido na seguinte fonte:
+
+  https://campushippo.com/lessons/how-to-convert-rgb-colors-to-hexadecimal-with-javascript-78219fdb
---
+++ script.js
@@ -0,0 +1,42 @@
+const sliders = document.querySelectorAll('.slider');
+const selectElementGreen = document.querySelector('#green');
+const selectElementBlue = document.querySelector('#blue');
+const result = document.querySelector('.rgb-result');
+
+let redNumber = '00';
+let greenNumber = '00';
+let blueNumber = '00';
+
+let redRadius = 0;
+let greenRadius = 0;
+let blueRadius = 0;
+
+// conversion of a rgb value to hexadecimal
+var convertRGBToHex = function (rgbNumber) {
+  var hex = Number(rgbNumber).toString(16);
+  if (hex.length < 2) {
+    hex = '0' + hex;
+  }
+  return hex;
+};
+
+for (let slider of sliders) {
+  slider.addEventListener('change', function (event) {
+    if (slider.id == 'red') {
+      redNumber = convertRGBToHex(slider.value);
+      redRadius = parseInt(slider.value);
+    } else if (slider.id == 'green') {
+      greenNumber = convertRGBToHex(slider.value);
+      greenRadius = parseInt(slider.value);
+    } else {
+      blueNumber = convertRGBToHex(slider.value);
+      blueRadius = parseInt(slider.value);
+    }
+
+    let radiusRGB = (redRadius + greenRadius + blueRadius) / (2 * 7.65);
+    console.log(radiusRGB);
+    let rgbResult = redNumber + greenNumber + blueNumber;
+    result.style.backgroundColor = '#' + rgbResult;
+    result.style.borderRadius = radiusRGB + '%';
+  });
+}
